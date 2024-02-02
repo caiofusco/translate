3fbf967c9102fad36e4e925ae61746745b7838b8
Caio dos Santos Fusco <caiofusco15@hotmail.com>
2024-02-01 23:56:40 -0300

    update main

--- main.js
+++ main.js
@@ -1,22 +1,8 @@
-const url = 'https://text-translator2.p.rapidapi.com/translate';
-const options = {
-	method: 'POST',
-	headers: {
-		'content-type': 'application/x-www-form-urlencoded',
-		'X-RapidAPI-Key': 'd38b50402emshc48d3746f0677f0p1c95bbjsn1445d5ed0956',
-		'X-RapidAPI-Host': 'text-translator2.p.rapidapi.com'
-	},
-	body: new URLSearchParams({
-		source_language: 'en',
-		target_language: 'pt',
-		text: 'What is your name?'
+fetch('https://libretranslate.com/translate')
+	.then(response => response.json())
+	.then(data => {
+		console.log(data);
 	})
-};
-
-try {
-	const response = await fetch(url, options);
-	const result = await options.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
+	.catch(error => {
+		console.error(error);
+	});
